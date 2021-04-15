a29d64e77b203e5f609fc7adf6abd2287786544c
talrejah02 <talrejah02@gmail.com>
2021-04-16 04:53:16 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+var  btnTranslate = document.querySelector("#btn-translate");
+var txtInput= document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+var url = "https://api.funtranslations.com/translate/minion.json"
+function gettranslated(text){
+  return url + "?" +"text=" + text
+}
+function errorhandler(){
+  console.log("soryy")
+}
+function clickhandler(){
+  var inputxt = txtInput.value
+  fetch(gettranslated(inputxt))
+  .then (response => response.json())
+  .then  (json => {
+    var translatedtext = json.contents.translated;
+    outputDiv.innerText = translatedtext;
+  })
+  .catch(errorhandler)
+};
+btnTranslate.addEventListener("click", clickhandler)
